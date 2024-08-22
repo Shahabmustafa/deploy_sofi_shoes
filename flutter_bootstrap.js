6044bb18947e5d5ba5f05f4bdce319a581da9d32
Shahabmustafa <shahabmustafa57@gmail.com>
2024-08-22 13:05:22 +0500

    uodate code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "662496348"
+    serviceWorkerVersion: "1727239063"
   }
 });
